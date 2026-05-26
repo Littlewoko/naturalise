644a71672e44522afd163d3743383386dc1c9acc
Bradley Watkins <bradley.watkins@dvla.gov.uk>
2026-05-26 14:07:46 +0100

    GREEN: checking for trailing whitespace and over length lines

--- src/rules/comment_rule.cpp
+++ src/rules/comment_rule.cpp
@@ -6,7 +6,29 @@ namespace {
             Violation violation;
             violation.level = ViolationLevel::WARN;
             violation.line_number = line_number;
-            violation.message = "comment line must begin with *";
+            violation.message = "line should begin with *";
+
+            violations.push_back(violation);
+        }
+    }
+
+    void check_trailing_whitespace(int line_number, const string &line, vector<Violation> &violations) {
+        if (!line.empty() && line.back() == ' ') {
+            Violation violation;
+            violation.level = ViolationLevel::WARN;
+            violation.line_number = line_number;
+            violation.message = "line should not contain trailing whitespace";
+
+            violations.push_back(violation);
+        }
+    }
+
+    void check_over_length(int line_number, const string &line, vector<Violation> &violations) {
+        if(!line.empty() && line.size() > 80) {
+            Violation violation;
+            violation.level = ViolationLevel::WARN;
+            violation.line_number = line_number;
+            violation.message = "line should not be greater than 80 characters";
 
             violations.push_back(violation);
         }
@@ -21,6 +43,9 @@ void CommentRule::validate(const Chunk &chunk, vector<Violation> &violations) {
     int line_number = chunk.starting_line;
     for (const string &line : chunk.lines) {
         check_comment_start(line_number, line, violations);
+        check_trailing_whitespace(line_number, line, violations);
+        check_over_length(line_number, line, violations);
+
         ++line_number;
     }
 }
--- tests/rules/comment_rule_test.cpp
+++ tests/rules/comment_rule_test.cpp
@@ -5,6 +5,22 @@
 
 using namespace std;
 
+const string _asterix_warning = "line should begin with *";
+const string _trailing_whitespace_warning = "line should not contain trailing whitespace";
+const string _over_length_line = "line should not be greater than 80 characters";
+
+auto has_violation = [](const vector<Violation> &violations, int line, const string &message, ViolationLevel level) {
+    for (auto const &v: violations) {
+        if (v.level == level 
+        && v.line_number == line
+        && v.message.find(message)) {
+            return true;
+        }
+    }
+
+    return false;
+};
+
 TEST_CASE("Comment Line Validator") {
     CommentRule rule;
 
@@ -54,5 +70,118 @@ TEST_CASE("Comment Line Validator") {
         REQUIRE(res.size() == 1);
         REQUIRE(res[0].level == ViolationLevel::WARN);
         REQUIRE(res[0].line_number == 50);
-        REQUIRE_THAT(res[0].message, Catch::Matchers::ContainsSubstring("must begin with *"));}
+        REQUIRE_THAT(res[0].message, Catch::Matchers::ContainsSubstring(_asterix_warning));    
+    }
+
+    SECTION("INVALID: empty string") {
+        vector<string> lines {""};
+
+        Chunk chunk;
+        chunk.starting_line = 50;
+        chunk.type = ChunkType::CommentBlock;
+        chunk.lines = lines;
+
+        vector<Violation> res;
+
+        rule.validate(chunk, res);
+
+        REQUIRE(res.size() == 1);
+        REQUIRE(res[0].level == ViolationLevel::WARN);
+        REQUIRE(res[0].line_number == 50);
+        REQUIRE_THAT(res[0].message, Catch::Matchers::ContainsSubstring(_asterix_warning));    
+    }
+
+    SECTION("INVALID: whitespace string") {
+        vector<string> lines {"    "};
+
+        Chunk chunk;
+        chunk.starting_line = 25;
+        chunk.type = ChunkType::CommentBlock;
+        chunk.lines = lines;
+
+        vector<Violation> res;
+
+        rule.validate(chunk, res);
+
+        bool has_trailing_whitespace = has_violation(res, chunk.starting_line, _trailing_whitespace_warning, ViolationLevel::WARN);
+        bool has_no_asterix = has_violation(res, chunk.starting_line, _asterix_warning, ViolationLevel::WARN);
+
+        REQUIRE(has_trailing_whitespace == true);    
+        REQUIRE(has_no_asterix == true);   
+    }
+
+    SECTION("INVALID: trailing whitespace") {
+        vector<string> lines {"* "};
+
+        Chunk chunk;
+        chunk.starting_line = 30;
+        chunk.type = ChunkType::CommentBlock;
+        chunk.lines = lines;
+
+        vector<Violation> res;
+
+        rule.validate(chunk, res);
+
+        REQUIRE(res.size() == 1);
+        REQUIRE(res[0].level == ViolationLevel::WARN);
+        REQUIRE(res[0].line_number == 30);
+        REQUIRE_THAT(res[0].message, Catch::Matchers::ContainsSubstring(_trailing_whitespace_warning));    
+    }
+
+    SECTION("INVALID: over length") {
+        string line(81, '*');
+        vector<string> lines {line};
+
+        Chunk chunk;
+        chunk.starting_line = 30;
+        chunk.type = ChunkType::CommentBlock;
+        chunk.lines = lines;
+
+        vector<Violation> res;
+
+        rule.validate(chunk, res);
+
+        REQUIRE(res.size() == 1);
+        REQUIRE(res[0].level == ViolationLevel::WARN);
+        REQUIRE(res[0].line_number == 30);
+        REQUIRE_THAT(res[0].message, Catch::Matchers::ContainsSubstring(_over_length_line));    
+    }
+
+    SECTION("VALID: at length") {
+        string line(80, '*');
+        vector<string> lines {line};
+
+        Chunk chunk;
+        chunk.starting_line = 30;
+        chunk.type = ChunkType::CommentBlock;
+        chunk.lines = lines;
+
+        vector<Violation> res;
+
+        rule.validate(chunk, res);
+
+        REQUIRE(res.size() == 0);
+    }
+
+    SECTION("INVALID: triple combo") {
+        string line(100, ' ');
+        vector<string> lines {line};
+
+        Chunk chunk;
+        chunk.starting_line = 30;
+        chunk.type = ChunkType::CommentBlock;
+        chunk.lines = lines;
+
+        vector<Violation> res;
+
+        rule.validate(chunk, res);
+
+        bool has_trailing_whitespace = has_violation(res, chunk.starting_line, _trailing_whitespace_warning, ViolationLevel::WARN);
+        bool has_no_asterix = has_violation(res, chunk.starting_line, _asterix_warning, ViolationLevel::WARN);
+        bool is_too_long = has_violation(res, chunk.starting_line, _over_length_line, ViolationLevel::WARN);
+
+        REQUIRE(has_trailing_whitespace == true);    
+        REQUIRE(has_no_asterix == true);  
+        REQUIRE(is_too_long == true);
+    }
 }
