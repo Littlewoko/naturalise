9a3ae41d7e79210bd93304a81b5268a04d222f42
Bradley Watkins <bradley.watkins@dvla.gov.uk>
2026-05-26 11:29:12 +0100

    REFACTOR: moved * check into private method

--- src/rules/comment_rule.cpp
+++ src/rules/comment_rule.cpp
@@ -1,14 +1,25 @@
 #include "comment_rule.h"
 
+namespace {
+    void check_comment_start(int line_number, const string &line, vector<Violation> &violations) {
+        if (line.empty() || line[0] != '*') {
+            Violation violation;
+            violation.line_number = line_number;
+            violation.message = "comment line must begin with *";
+
+            violations.push_back(violation);
+        }
+    }
+}
+
 bool CommentRule::should_run(ChunkType type) {
     return type == ChunkType::CommentBlock;
 }
 
 void CommentRule::validate(const Chunk &chunk, vector<Violation> &violations) {
-    if (chunk.type == ChunkType::CommentBlock) {
-        Violation violation;
-        violation.line_number = 0;
-        violation.message = "abc";
-        violations.push_back(violation);
+    int line_number = chunk.starting_line;
+    for (const string &line : chunk.lines) {
+        check_comment_start(line_number, line, violations);
+        ++line_number;
     }
 }
--- tests/rules/comment_rule_test.cpp
+++ tests/rules/comment_rule_test.cpp
@@ -1,4 +1,5 @@
 #include <catch2/catch_test_macros.hpp>
+#include <catch2/matchers/catch_matchers_string.hpp>
 #include "rules/comment_rule.h"
 #include <string>
 
@@ -23,9 +24,34 @@ TEST_CASE("Comment Line Validator") {
         REQUIRE(res == false);
     }
 
-    // SECTION("VALID: Line must begin with a *") {
-    //     vector<string> lines {"*"};
+    SECTION("VALID: Line must begin with a *") {
+        vector<string> lines {"*"};
 
-       
-    // }
+        Chunk chunk;
+        chunk.starting_line = 0;
+        chunk.type = ChunkType::CommentBlock;
+        chunk.lines = lines;
+
+        vector<Violation> res;
+
+        rule.validate(chunk, res);
+
+        REQUIRE(res.size() == 0);
+    }
+
+    SECTION("INVALID: Line must begin with a *") {
+        vector<string> lines {"hello"};
+
+        Chunk chunk;
+        chunk.starting_line = 50;
+        chunk.type = ChunkType::CommentBlock;
+        chunk.lines = lines;
+
+        vector<Violation> res;
+
+        rule.validate(chunk, res);
+
+        REQUIRE(res.size() == 1);
+        REQUIRE(res[0].line_number == 50);
+        REQUIRE_THAT(res[0].message, Catch::Matchers::ContainsSubstring("must begin with *"));}
 }
